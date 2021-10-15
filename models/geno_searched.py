a7ccecd1b5ba20760090cfa615c93e5c530cc017
cccchenccc <cccchenccc@163.com>
2021-10-16 08:24:41 +1100

    CODE REFACTOR

--- models/geno_searched.py
+++ models/geno_searched.py
@@ -7,5 +7,5 @@ senas_node_3 = Genotype(down=[('se_conv_3', 1), ('dil_2_conv_5', 0), ('dil_3_con
 senas_node_4 = Genotype(down=[('se_conv_3', 1), ('avg_pool', 0), ('dil_3_conv_5', 2), ('dep_sep_conv_5', 1), ('dil_3_conv_5', 2), ('avg_pool', 0), ('avg_pool', 1), ('dil_3_conv_5', 3)], down_concat=range(2, 6), up=[('up_sample', 1), ('dil_3_conv_5', 0), ('dil_3_conv_5', 0), ('dil_2_conv_5', 2), ('dil_3_conv_5', 1), ('dil_2_conv_5', 2), ('dep_sep_conv_3', 0), ('dil_2_conv_5', 4)], up_concat=range(2, 6), gamma=[0, 0, 0, 1, 1, 1])
 
 
-senas = senas_node_3
+senas = senas_node_4
 
