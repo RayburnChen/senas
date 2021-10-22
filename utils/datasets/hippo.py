5c42352bd96caf2b5109e594bb9505c46373294f
cccchenccc <cccchenccc@163.com>
2021-10-22 20:17:55 +1100

    CODE REFACTOR

--- utils/datasets/hippo.py
+++ utils/datasets/hippo.py
@@ -16,7 +16,7 @@ from PIL import Image
 
 class Hippo(BaseDataset):
 
-    BASE_DIR, NUM_CLASS, CROP_SIZE = ('Task04_Hippocampus/', 2, (56, 40))
+    BASE_DIR, NUM_CLASS, CROP_SIZE = ('Task04_Hippocampus/', 2, 64)
     IN_CHANNELS = 1
     CLASS_WEIGHTS = None
     mean = [0.79002064]
